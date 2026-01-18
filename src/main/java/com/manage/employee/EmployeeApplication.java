9cd56291f7107ec9c8ab0bb469d8618304018fea
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-01-18 18:53:26 +0530

    Update EmployeeApplication.java

--- src/main/java/com/manage/employee/EmployeeApplication.java
+++ src/main/java/com/manage/employee/EmployeeApplication.java
@@ -1,5 +1,10 @@
 package com.manage.employee;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
