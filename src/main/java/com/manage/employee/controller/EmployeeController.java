f473a7992d73205b991b768b9cfb1f3d2a2b0423
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-01-19 09:34:53 +0530

    Update EmployeeController.java
    Commit #19012026

--- src/main/java/com/manage/employee/controller/EmployeeController.java
+++ src/main/java/com/manage/employee/controller/EmployeeController.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(
+        name = "REST API for EMS",
+        description = "CRUD API FOR EMS"
+)
 @RestController
 @RequestMapping("api/employee")
 public class EmployeeController {
@@ -21,6 +25,14 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    @Operation(
+            summary = "CREATE API",
+            description = "CREATING A EMPLOYEE"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "EMPLOYEE CREATED"
+    )
     @PostMapping("create")
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody @Valid EmployeeDto employee){
         //System.out.println("createEmployee "+employee);
@@ -28,30 +40,63 @@ public class EmployeeController {
         return new ResponseEntity<>(createdEmployee, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "RETRIEVE API",
+            description = "RETRIEVE A EMPLOYEE"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "EMPLOYEE FOUND"
+    )
     @GetMapping("get")
     public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
         List<EmployeeDto> employeeList = employeeService.getAllEmployees();
         return new ResponseEntity<>(employeeList, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "RETRIEVE API",
+            description = "RETRIEVE ALL EMPLOYEES"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "EMPLOYEES FOUND"
+    )
     @GetMapping("get/{id}")
     public ResponseEntity<EmployeeDto> getEmployee(@PathVariable Long id){
         EmployeeDto employee = employeeService.getEmployee(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "UPDATE API",
+            description = "UPDATE A EMPLOYEE"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "EMPLOYEE UPDATED"
+    )
     @PutMapping("update/{id}")
     public ResponseEntity<EmployeeDto> updateEmployee(@RequestBody @Valid EmployeeDto employee,@PathVariable Long id){
         employee.setId(id);
         EmployeeDto updatedEmployee = employeeService.updateEmployee(employee);
         return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
     }
+
+    @Operation(
+            summary = "DELETE API",
+            description = "DELETE A EMPLOYEE"
+    )
     @DeleteMapping("delete/{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable Long id){
         employeeService.deleteEmployee(id);
         return new ResponseEntity<>("Employee deleted with id: "+id, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "DELETE API",
+            description = "DELETE ALL EMPLOYEES"
+    )
     @DeleteMapping("deleteAll")
     public ResponseEntity<String> deleteAllEmployees(){
         employeeService.deleteAllEmployees();
