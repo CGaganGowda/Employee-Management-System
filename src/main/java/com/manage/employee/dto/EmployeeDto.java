c27a596535d68eb08a49ef5522fa73b2bf61089a
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-01-19 09:37:42 +0530

    Update EmployeeDto.java
    DTO #19012026

--- src/main/java/com/manage/employee/dto/EmployeeDto.java
+++ src/main/java/com/manage/employee/dto/EmployeeDto.java
@@ -11,6 +11,9 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(
+       description = "EMPLOYEE-DETAIL"
+)
 public class EmployeeDto {
     //--------------------VALIDATIONS-----------------------------
     //Validation Bean Annotations
