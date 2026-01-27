38fe0e9d73b504a6f076ca69f1bac73ef94a6d3e
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-01-27 10:26:31 +0530

    Update EmployeeRepo.java

--- src/main/java/com/manage/employee/repo/EmployeeRepo.java
+++ src/main/java/com/manage/employee/repo/EmployeeRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-
+    //2 Custom methods for validating Employee email (name - optional --- for partners). Org email has to be unique.
     Optional<Employee> findByEmail(String email);
     Optional<Employee> findByName(String name);
 }
