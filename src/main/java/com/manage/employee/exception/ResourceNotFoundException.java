78574de6d7a213b59f48b000ed5b40357554af64
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-01-27 10:22:35 +0530

    Update ResourceNotFoundException.java

--- src/main/java/com/manage/employee/exception/ResourceNotFoundException.java
+++ src/main/java/com/manage/employee/exception/ResourceNotFoundException.java
@@ -8,7 +8,7 @@ public class ResourceNotFoundException extends RuntimeException{
     private String resourceName;
     private String fieldName;
     private Long fieldValue;
-
+//Fields are not mandatory - a simple message onto the console is good(optional to console). Message to the frontend is mandatory. 
     public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue) {
         super(String.format("%s not found in %s : '%s'",resourceName, fieldName, fieldValue));
         this.resourceName = resourceName;
