f77dcc7fb2880ea85ebe0e5bfe216f3a1feb28ba
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-02-03 21:52:24 +0530

    Update MapperClass.java

--- src/main/java/com/manage/employee/mapper/MapperClass.java
+++ src/main/java/com/manage/employee/mapper/MapperClass.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+//Can be replaced with a external dependency - ModelMapper
+
 @Component
 public class MapperClass {
 
