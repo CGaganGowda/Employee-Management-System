62bab40874d596e5ab5517597ae055eed3aa9e16
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-02-27 22:43:57 +0530

    Update RoleRepo

--- src/main/java/com/manage/employee/repo/RoleRepo.java
+++ src/main/java/com/manage/employee/repo/RoleRepo.java
@@ -1,9 +1,11 @@
 package com.manage.employee.repo;
 
-import com.Management.todo.model.Role;
+
+import com.manage.employee.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepo extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
+
 
