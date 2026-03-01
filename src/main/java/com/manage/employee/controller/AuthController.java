707bf3e965b82caf997b6dffd96cde74330886d3
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-03-01 22:01:50 +0530

    Add files via upload

---
+++ src/main/java/com/manage/employee/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.Management.todo.controller;
+
+import com.Management.todo.dto.LoginDto;
+import com.Management.todo.dto.UserDto;
+import com.Management.todo.model.User;
+import com.Management.todo.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+
+    public AuthService authService;
+
+    @PostMapping("register")
+    public ResponseEntity<User> registerUser(@RequestBody UserDto userDto){
+        User user = authService.register(userDto);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<String> loginUser(@RequestBody LoginDto  loginDto){
+       String response =  authService.login(loginDto);
+       return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
