2b620a998dcc6a4dde35b5a1fa45be2996cc5677
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-03-04 22:41:15 +0530

    Update AuthService.java

--- src/main/java/com/manage/employee/service/AuthService.java
+++ src/main/java/com/manage/employee/service/AuthService.java
@@ -1,11 +1,12 @@
 package com.manage.employee.service;
 
-import com.manage.employee.service.dto.LoginDto;
-import com.manage.employee.service.dto.UserDto;
-import com.manage.employee.service.model.User;
+import com.manage.employee.dto.LoginDto;
+import com.manage.employee.dto.UserDto;
+import com.manage.employee.model.User;
 
 public interface AuthService {
     User register(UserDto userDto);
     String login(LoginDto loginDto);
 }
+
 
