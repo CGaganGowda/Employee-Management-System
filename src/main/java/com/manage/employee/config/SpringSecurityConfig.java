fd93c03eb3004156596a5a7f1cbd2e2613dec37e
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-04-18 19:41:19 +0530

    Add @AllArgsConstructor annotation to SpringSecurityConfig

--- src/main/java/com/manage/employee/config/SpringSecurityConfig.java
+++ src/main/java/com/manage/employee/config/SpringSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@AllArgsConstructor
 @EnableMethodSecurity
 @Configuration
 public class SpringSecurityConfig {
